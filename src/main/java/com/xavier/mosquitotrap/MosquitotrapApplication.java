6f8548dd3e5c6270d992a325e8015cb6714b5aa2
Xavier Francisco Nhagumbe <xavierfrancisco353@gmail.com>
2019-09-11 10:53:42 +0200

    Initialize

---
+++ src/main/java/com/xavier/mosquitotrap/MosquitotrapApplication.java
@@ -0,0 +1,13 @@
+package com.xavier.mosquitotrap;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MosquitotrapApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MosquitotrapApplication.class, args);
+	}
+
+}
